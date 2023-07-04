702409275a4f75635cf1197ab00525b72d36afb5
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-04 10:19:45 +0300

    Enum's apple, projecStatus class are created

---
+++ m2-enum/src/main/java/com/cydeo/Apple.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public class Apple {
+    int weight;
+
+    public Apple(int weight) {
+        this.weight = weight;
+    }
+}
--- m2-enum/src/main/java/com/cydeo/Operation.java
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -1,5 +1,15 @@
 package com.cydeo;
 
 public enum Operation {
-    PLUS,MINUS,MULTIPLE,DIVIDE
+    PLUS(1),MINUS(4),MULTIPLE(11),DIVIDE(66);
+
+ private int value;
+
+    Operation(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
