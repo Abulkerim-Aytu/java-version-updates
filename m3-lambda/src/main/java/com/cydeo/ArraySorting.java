204f92cbc4d2cd31ed06dd2c570e8e793d4441a3
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-04 13:30:21 +0300

    Lambda's Tesks are Updated.

--- m3-lambda/src/main/java/com/cydeo/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -3,10 +3,15 @@ package com.cydeo;
 public class ArraySorting {
     public static void main(String[] args) {
         ArraySorting as = new ArraySorting();
-        as.sort();
 
+        QuickSort qs = new QuickSort();
+        BubbleSorting bs = new BubbleSorting();
+
+        as.sort(qs);
+        as.sort(bs);
     }
-    private void sort(){
-        System.out.println("Sorting array");
+    private void sort(Sorting sorting){
+        sorting.sort();
     }
+
 }
