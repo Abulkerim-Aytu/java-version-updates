18b1a8a65e84265414b81b3e0faa0e6b7b251168
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-04 16:50:25 +0300

    Lambda's inventory Tesks are Complited.

--- m3-lambda/src/main/java/com/cydeo/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -7,9 +7,16 @@ public class AppleTest {
     public static void main(String[] args) {
         List<Apple> inventory = new ArrayList<Apple>();
         inventory.add(new Apple(300, Color.GREEN));
-        inventory.add(new Apple(200, Color.RED));
+        inventory.add(new Apple(250, Color.RED));
         inventory.add(new Apple(190, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
+
+        AppleHeavyPredicate abs = new AppleHeavyPredicate();
+        List<Apple> heavyApple= filterApple(inventory,abs);
+        System.out.println(heavyApple);
+
+        List<Apple> greenApple= filterApple(inventory,new AppleGreenColorPredicate());
+        System.out.println(greenApple);
     }
 
     private static List<Apple> filterApple(List<Apple> inventory, ApplePredicate applePredicate) {
