217ff876d895dc8e77ca1944fdaef4a6d499d7fb
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-04 15:37:38 +0300

    Lambda's Tesks are Updated.

---
+++ m3-lambda/src/main/java/com/cydeo/Apple.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
+public class Apple {
+    private int weight;
+    private Color color;
+}
