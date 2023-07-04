76821b4f857ec05fb7f2680d453e01c01f00259a
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-04 13:21:44 +0300

    Lambda's Tesks are created.

---
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        ArraySorting as = new ArraySorting();
+        as.sort();
+
+    }
+    private void sort(){
+        System.out.println("Sorting array");
+    }
+}
---
+++ m3-lambda/src/main/java/com/cydeo/BubbleSorting.java
@@ -0,0 +1,8 @@
+package com.cydeo;
+
+public class BubbleSorting implements Sorting{
+    @Override
+    public void sort() {
+        System.out.println("Quick Sorting");
+    }
+}
