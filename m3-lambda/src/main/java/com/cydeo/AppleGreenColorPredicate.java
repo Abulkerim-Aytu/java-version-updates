9f19c42d3645f05697ac09acc72248bbf90d554b
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-04 15:50:20 +0300

    Lambda's inventory Tesks are created.

---
+++ m3-lambda/src/main/java/com/cydeo/AppleGreenColorPredicate.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public class AppleGreenColorPredicate implements ApplePredicate {
+
+    @Override
+    public boolean test(Apple apple) {
+        return apple.getColor().equals(Color.GREEN);
+    }
+}
