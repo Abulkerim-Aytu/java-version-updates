5b5537c4e7420a66b1714d77b7c5083d47648c38
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-10 16:40:47 +0300

    Double Clon Operator updated and complited

--- m4_double_colon_operator/src/main/java/com/cydeo/Main.java
+++ m4_double_colon_operator/src/main/java/com/cydeo/Main.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -27,6 +28,9 @@ public class Main {
 
         Function<Integer,Double> b = new MyClass()::methods;
         BiFunction<MyClass,Integer,Double> b1=MyClass::methods;
+
+        Consumer<Integer> display= i -> System.out.println(i);
+        Consumer<Integer> display2= System.out::println;
 
 
 
