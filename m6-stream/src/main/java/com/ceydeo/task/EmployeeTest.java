e7640d509ceef2b3856d8dbd0650274c228f5509
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-07-12 10:55:23 +0300

    Stream's float() maps tasks are updated

--- m6-stream/src/main/java/com/ceydeo/task/EmployeeTest.java
+++ m6-stream/src/main/java/com/ceydeo/task/EmployeeTest.java
@@ -3,5 +3,16 @@ package com.ceydeo.task;
 public class EmployeeTest {
     public static void main(String[] args) {
 
+        System.out.println("Print All Emails");
+        EmployeeData.readAll()
+                .map(Employee::getEmpEmail)
+                .forEach(System.out::println);
+
+        System.out.println("Print All Phone Numbers");
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+
     }
 }
